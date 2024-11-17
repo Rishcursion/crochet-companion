39ee3ea2ddf15001634c7a7415947f5e2054b6f4
Rish <2004rish@gmail.com>
2024-11-17 18:17:30 +0530

    chore: setting up project structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<main class="text-black text-2xl">
+<h1>Hello World</h1>
+</main>
+
